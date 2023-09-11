245c4076e54f1c362a004c5ef1107091500a940d
dc.To <sdoz@live.com>
2023-09-11 18:00:14 +0800

    update cors pipeline

--- app/Pipeline/Cors.php
+++ app/Pipeline/Cors.php
@@ -4,13 +4,19 @@ namespace App\Pipeline;
 
 class Cors extends \VM\Pipeline {
 
-
+    /**
+     * Cors domain request
+     */
     public function handle(\VM\Http\Request $request, \Closure $next, ...$guards)
     {
         if ($request->method('OPTIONS')) {
-            
+            return $next(make('response')->make()->withHeaders([
+                'Access-Control-Allow-Origin'=>$request->header('origin'),
+                'Access-Control-Allow-Methods'=>'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers'=>'Accept, Application, Authorization, Content-Type, Origin, X-Requested-With',
+                'Access-Control-Allow-Credentials'=>'true'
+            ]));
         }
-
         return $next($request);
     }
     
