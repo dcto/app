89d123d2d72698c1d5ca2cf2c1c138c6014814d5
dc.To <sdoz@live.com>
2023-09-11 16:38:46 +0800

    update model

--- model/Model.php
+++ model/Model.php
@@ -23,27 +23,8 @@ abstract class Model extends \VM\Model
     public $timestamps = true;
 
     /**
-     * @var bool
-     * The Database SoftDelete Attribute
-     */
-    public $softDelete = true;
-
-    /**
      * @var array
      * Auto hidden deleted_at 
      */
     protected $hidden = ['deleted_at'];
-
-    /**
-     * common down table
-     */
-    static public function down()
-    {
-        if(!getenv('ENV')) return false;
-
-        if(config('database.default') == 'mysql') \DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        
-        return \Schema::dropIfExists(static::table());
-    }
-    
 }
