29c87d97f97892727e27c097a70af2defa0b5359
dc.To <sdoz@live.com>
2023-09-12 14:09:22 +0800

    update cors pipeline

--- app/Pipeline/Cors.php
+++ app/Pipeline/Cors.php
@@ -11,6 +11,7 @@ class Cors extends \VM\Pipeline {
     {
         if ($request->method('OPTIONS')) {
             return $next(make('response')->make()->withHeaders([
+                'Access-Control-Max-Age'=>'600',
                 'Access-Control-Allow-Origin'=>$request->header('origin'),
                 'Access-Control-Allow-Methods'=>'GET, POST, PUT, DELETE, OPTIONS',
                 'Access-Control-Allow-Headers'=>'Accept, Application, Authorization, Content-Type, Origin, X-Requested-With',
