3ab57c573228c2c9ab59ab25e833e16327327d75
dc.To <sdoz@live.com>
2023-09-18 09:43:09 +0800

    update dml

--- app/Traits/DMLTrait.php
+++ app/Traits/DMLTrait.php
@@ -20,10 +20,10 @@ trait DMLTrait {
     */
     public function create()
     {
-        if($this->model()::create($this->input(__METHOD__))){
-            return $this->response(lang('msg.create.ok', $this->lang));
+        if($this->model::create($this->input(__METHOD__))){
+            return $this->response(lang('e.create.ok', $this->lang));
         }else{
-            return $this->response(lang('msg.crate.no', $this->lang), $this->code + 1 );
+            return $this->response(lang('e.crate.no', $this->lang), $this->code + 1 );
         }
     }
 
@@ -36,10 +36,10 @@ trait DMLTrait {
     */
     public function update()
     {
-        if($this->model()::where($this->where(__METHOD__))->update($this->input(__METHOD__))){
-            return $this->response(lang('msg.update.ok', $this->lang));
+        if($this->model::where($this->where(__METHOD__))->update($this->input(__METHOD__))){
+            return $this->response(lang('e.update.ok', $this->lang));
         }else{
-            return $this->response(lang('msg.update.no', $this->lang), $this->code + 2);
+            return $this->response(lang('e.update.no', $this->lang), $this->code + 2);
         }
     }
 
@@ -47,16 +47,17 @@ trait DMLTrait {
     /**
     * Common delete
     * @param  void 
+    
     * @return void 
     * @author  dc.To
     * @version 20230724
     */
     public function delete()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
     {
-        if($this->model()::where($this->where(__METHOD__))->delete()){
-            return $this->response(lang('msg.delete.ok', $this->lang));
+        if($this->model::where($this->where(__METHOD__))->delete()){
+            return $this->response(lang('e.delete.ok', $this->lang));
         }else{
-            return $this->response(lang('msg.delete.no', $this->lang), $this->code + 3 );
+            return $this->response(lang('e.delete.no', $this->lang), $this->code + 3 );
         }
     }
 }
