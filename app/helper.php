ffb973b9f8f79f06d50661844957593009aa81d6
dc.To <sdoz@live.com>
2023-10-17 15:49:59 +0800

    update helpers

--- app/helper.php
+++ app/helper.php
@@ -1,50 +1,23 @@
 <?php
-/**
- * 全局响应
- * @param string|array $response 响应内容 
- * @param int $code 错误码
- * @return \VM\Http\Response
- */
-function response($response = null, $code = 0, $message = null, $callback = null)
-{
-    if(is_null($response)) return make('response');
-    
-    $dataset = [];
-    is_string($response) ?  $message = $response : $dataset = $response;
-    unset($response);
-    return Response::json(
-        array(
-            'code' => $code,  
-            'dataset' => $dataset,
-            'message' => $message,
-            'callback' => $callback
-        )
-    );
-}
 
 /**
  * 视图响应
  * @param string $layout
- * @param array $data
- * @param int $status
- * @param array $headers
+ * @param array|object $dataset
  * @return \VM\Http\Response
  */
-if(!function_exists('template')) {
-    function template($layout, $data = [], $status = 200, array $headers = [])
-    {
-        return make('response')->html(
-            app('view')->make('php')
-            ->render($layout, $data)
-        , $status, $headers);
-    }
+function view($layout, ...$dataset)
+{
+    return make('response')->html(app('view')->make('php')->render($layout, ...$dataset));
 }
 
 /**
- * alias name for template
+ * www path
+ * @param string ...$paths
+ * @return string
  */
-if(!function_exists('view')){
-    function view($layout, $data = [], $status = 200, array $headers = []){
-        return template($layout, $data, $status, $headers);
-    }
+function www(...$paths)
+{
+    $file =  str_replace('//', '/', '/'. (count($paths) == 1 ? trim($paths[0], '/') : join('/', $paths)));
+    return is_file(_WWW_.$file) ? $file : '/images/404.gif';
 }
