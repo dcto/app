375ff95692f6ef4341531571c4c6766563de30b0
dc.To <sdoz@live.com>
2023-10-17 15:45:57 +0800

    add service

---
+++ app/Service/Service.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Service;
+
+/**
+* @package Service
+*/
+class Service extends \VM\Services\ServiceProvider 
+{
+
+    /**
+    * Boot service.
+    * @param  void 
+    * @return void 
+    */
+    public function boot()
+    {
+
+    }
+
+    /**
+    * Register service.
+    * @param  void 
+    * @return void 
+    */
+    public function register()
+    {
+
+    }
+}
