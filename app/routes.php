e15ae55185763186920d413d2723c3a11e36c2d8
dc.To <sdoz@live.com>
2023-10-22 14:55:21 +0800

    update routes

--- app/routes.php
+++ app/routes.php
@@ -1,21 +1,22 @@
 <?php
 //公共组
 Router::group( ['id' => 'public', 'prefix' => '/', 'namespace' => 'App\Controller'], function () {  
-    Router::any( '/', fn()=>response()->make("<pre>".print_r($_ENV, true)."</pre>"));
-    Router::any( '/test')->call('Test@index');
-    Router::any( '/lang')->call('Lang@index');
-    Router::any( '/session')->call('Session@index');
-    Router::any( '/cookie')->call('Cookie@index');
-    Router::any( '/file' )->call( 'Test@files');
-    Router::any( '/test/(list:*)/(id:\d+)' )->call( 'Test@test' );
-    Router::get( '/test/(shop:vip|user)' )->call( 'Test@shop' ); //only allow vip or user string
-    Router::get( '/test/(shop:vip|user)/(id:|\d+)' )->call( 'Test@shop' );
+    Router::any('/', response('index'));
+    Router::any('/test')->call('Test@index');
+    Router::any('/lang')->call('Lang@index');
+    Router::any('/session')->call('Session@index');
+    Router::any('/cookie')->call('Cookie@index');
+    Router::any('/file' )->call('Test@files');
+    Router::any('/twig' )->call('Test@twig');
+    Router::any('/test/(list:*)/(id:\d+)' )->call('Test@test' );
+    Router::get('/test/(shop:vip|user)' )->call('Test@shop' ); //only allow vip or user string
+    Router::get('/test/(shop:vip|user)/(id:|\d+)' )->call('Test@shop' );
     //注册
-    Router::post( '/signup' )->call( 'User@register' );
+    Router::post('/signup' )->call('User@register' );
     //登录
-    Router::post( '/signin' )->call( 'User@login' );
+    Router::post('/signin' )->call('User@login' );
     //登出
-    Router::get( '/logout' )->call( 'User@logout' );
+    Router::get('/logout' )->call('User@logout' );
 
     //Restful CRUD
     //Router::restful('/user')->call( 'User@restful');
