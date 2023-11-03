591ffc1c528871e031a61703195e1752b2c20154
dc.To <sdoz@live.com>
2023-11-03 21:56:36 +0800

    remove the www helper

--- app/helper.php
+++ app/helper.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * 视图响应
  * @param string $template
@@ -13,15 +12,4 @@ function view($template, ...$dataset)
         throw new \App\Exception\ErrorException('Please using `composer install varimax/view` @link https://packagist.org/packages/varimax/view ');
     }
     return make('response')->html(app('view')->render($template, ...$dataset));
-}
-
-/**
- * www path
- * @param string ...$paths
- * @return string
- */
-function www(...$paths)
-{
-    $file =  str_replace('//', '/', '/'. (count($paths) == 1 ? trim($paths[0], '/') : join('/', $paths)));
-    return is_file(_WWW_.$file) ? $file : '/images/404.gif';
 }
