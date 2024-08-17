03025f3c752e78a50bb13d64aedd31d7fd26a082
dc.To <sdoz@live.com>
2024-08-17 15:35:26 +0800

    add Index controller

---
+++ app/Controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+/**
+ * Index controller
+ * @package app
+ * @author  dc.To
+ * @version 20240817
+ * @copyright ©2024 varimax team all rights reserved.
+ */
+class Index extends Controller {
+
+    public function index() {
+        return 'Hello Varimax!';
+    }
+}
