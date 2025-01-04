146fd7c0ae2d9b2ab97ab7a71e0d357485516231
dc.To <sdoz@live.com>
2025-01-04 11:52:07 +0800

    add provider file

---
+++ provider/Provider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Provider;
+
+/**
+* @package Service
+*/
+class Provider extends \VM\Services\ServiceProvider 
+{
+    /**
+    * Register service.
+    * @param  void 
+    * @return void 
+    */
+    public function register()
+    {
+
+    }
+}
